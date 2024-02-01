68044913e29ed51fc801e6a206b13c2446040196
SyedSalman007 <97502374+SyedSalman007@users.noreply.github.com>
2024-02-01 21:25:00 +0500

    final

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "My narrative is one of exploration and commitment to shaping the future by integrating cutting-edge technologies like Azure into the landscape of machine learning and biotechnology.";
 
 const About = () => {
   return (
